8ccd64f18625eb5da6c467630bd313f507d5e795
AstridG <95365617+stridouille@users.noreply.github.com>
2024-07-11 13:30:52 +0200

    Feat enhanced navigation (#2)
    * feat: eart only moves when grabbed
    
    * add zooming function possibilities
    
    * edit zoom2
    
    * add zoom relative and absolute (not fully properly working yet)
    
    * fix: move enable/disable button in upper panel

--- Assets/Scripts/PanZoomOrbit.cs
+++ Assets/Scripts/PanZoomOrbit.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.EventSystems;
 
 public class PanZoomOrbitCenter : MonoBehaviour
@@ -8,10 +11,17 @@ public class PanZoomOrbitCenter : MonoBehaviour
     [SerializeField] private GameObject parentModel;
     [SerializeField, Range(100f, 700f)] float rotationSpeed = 500.0f;
     private Vector3 mouseWorldPosStart;
-    private float zoomScale = 10f;
-    private float zoomMin = 0.5f;
-    private float zoomMax = 100.0f;
-    private float defaultFieldOfView = 60.0f;
+    private float zoomScale = 0.5f;
+    private float zoomMin = 0.05f;
+    private float zoomMax = 2f;
+    private float defaultOrthoSize = 1f;
+    private bool dragBegunOnSpheres = false;
+    Camera m_Camera;
+
+    void Awake()
+    {
+       m_Camera = Camera.main;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +32,33 @@ public class PanZoomOrbitCenter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check for Orbit, Pan, Fit, Zoom
-        if (Input.GetKey(KeyCode.Mouse0) && !EventSystem.current.IsPointerOverGameObject()) //Check for orbit
-        {
-            CamOrbit();
+
+        if (Input.GetMouseButtonDown(0)) {          //check for beginning of orbit
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider.transform.parent.gameObject == parentModel) {
+                    dragBegunOnSpheres = true;
+                }
+            }
         }
-        if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject()) //Check for Pan
-        {
-            mouseWorldPosStart = GetPerspectivePos();
+
+        if (Input.GetMouseButton(0)) { //Check for orbit
+            if (dragBegunOnSpheres) {
+                CamOrbit();
+            }
         }
-        if (Input.GetMouseButton(1) && !EventSystem.current.IsPointerOverGameObject())
-        {
-            Pan();
+
+        if (Input.GetMouseButtonUp(0)) {
+            // TODO: reduce the speed and continue camorbit to give a sense of movement
+            dragBegunOnSpheres = false;
         }
-        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        ZoomRelative(Input.GetAxis("Mouse ScrollWheel"));
     }
 
 
-    private void CamOrbit()
+    public void CamOrbit()
     {
         if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
         {
@@ -50,54 +69,120 @@ public class PanZoomOrbitCenter : MonoBehaviour
         }
     }
 
-    void Pan()
-    {
-        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
-        {
-            Vector3 mouseWorldPosDiff = mouseWorldPosStart - GetPerspectivePos();
-            Camera.main.transform.position += mouseWorldPosDiff;
-        }
-    }
-
-    private Bounds GetBound(GameObject parentGameObject) {
-        Bounds bound = new Bounds(parentGameObject.transform.position, Vector3.zero);
-        var rList = parentGameObject.gameObject.GetComponentsInChildren<Renderer>();
-        foreach (Renderer r in rList) {
-            bound.Encapsulate(r.bounds);
-        }
-        return bound;
-    }
-
     public void FitToScreen() {
-        Camera.main.fieldOfView = defaultFieldOfView;
-        Bounds bound = GetBound(parentModel);
-        Vector3 boundSize = bound.size;
-        float boundDiagonal = Mathf.Sqrt(Mathf.Pow(boundSize.x, 2) + Mathf.Pow(boundSize.y, 2) + Mathf.Pow(boundSize.z, 2));
-        float camDistanceToBoundCenter = boundDiagonal / 2 / (Mathf.Tan(Camera.main.fieldOfView / 2 * Mathf.Deg2Rad));
-        float camDistanceToBoundWithOffset = camDistanceToBoundCenter + boundDiagonal / 2.0f - (Camera.main.transform.position - transform.position).magnitude;
-        Camera.main.transform.position = bound.center - transform.forward * camDistanceToBoundWithOffset;
-        camDistanceToBoundWithOffset = camDistanceToBoundCenter + boundDiagonal / 2.0f - (Camera.main.transform.position - transform.position).magnitude;
-        Camera.main.transform.position = bound.center - transform.forward * camDistanceToBoundWithOffset;
+        m_Camera.orthographicSize = defaultOrthoSize;
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
-    void Zoom(float zoomDiff)
+
+    // Zoom and adjust camera by simple linear transformation
+    public void ZoomNaive(float zoomDiff)
     {
         if (zoomDiff != 0)
         {
             mouseWorldPosStart = GetPerspectivePos();
-            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - zoomDiff * zoomScale, zoomMin, zoomMax);
-            Vector3 mouseWorldPosDiff = mouseWorldPosStart - GetPerspectivePos();
-            Camera.main.transform.position += mouseWorldPosDiff;
+            m_Camera.orthographicSize = Mathf.Clamp(m_Camera.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+            gameObject.transform.position += mouseWorldPosStart - GetPerspectivePos();
         }
     }
 
+
+    //Zoom and adjust camera angle by rotating by the angle difference between the point on mouse position before and after zoom
+    void ZoomRelative(float zoomDiff)
+    {
+        if (zoomDiff != 0)
+        {
+            Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Get the pointed pixel transform in world space before zoom
+                Vector3 mouseWorldPosBefore = hit.point;
+                // Adjust the screen box
+                m_Camera.orthographicSize = Mathf.Clamp(m_Camera.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+                ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    // Get the pointed pixel transform in world space before zoom
+                    Vector3 mouseWorldPosAfter = hit.point;
+                    DiffSpherical(mouseWorldPosAfter, mouseWorldPosBefore, 0.5f, out float diffX, out float diffY);
+                    gameObject.transform.Rotate(-diffX, diffY, 0);
+                }
+            } else {
+                m_Camera.orthographicSize = Mathf.Clamp(m_Camera.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+            }
+        }
+    }
+
+    // Zoom and adjust camera angle by computing the right absolute angle
+    void ZoomAbsolute(float zoomDiff)
+    {
+        if (zoomDiff != 0)
+        {
+            Vector3 mouseScreenPos = Input.mousePosition;
+            Ray ray = m_Camera.ScreenPointToRay(mouseScreenPos);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log("pos screen mouse : " + mouseScreenPos);
+                // Get the pointed pixel transform in world space before zoom
+                Vector3 mouseWorldPos = hit.point;
+                Debug.Log("pos world mouse : " + mouseWorldPos);
+
+                // Adjust the screen box
+                m_Camera.orthographicSize = Mathf.Clamp(m_Camera.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+                //float s = Mathf.Clamp(m_Camera.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+                
+                // Define the problem's parameters
+                int h = Screen.height;
+                int w = Screen.width;
+                float s = m_Camera.orthographicSize;
+                float r = 0.5f;
+                float phi = Mathf.Asin(mouseWorldPos.y/r) * Mathf.Rad2Deg;
+                if (mouseWorldPos.z > 0)
+                {
+                    phi = 180 - phi;
+                }
+                Debug.Log("phi : " + phi);
+                float psi = Mathf.Asin(mouseWorldPos.x/r) * Mathf.Rad2Deg;
+                if (mouseWorldPos.z > 0)
+                {
+                    psi = 180 - psi;
+                }
+                Debug.Log("psi : " + psi);
+                float alpha = (float)h/w;
+                
+                float theta = - Mathf.Asin(2*s*(mouseScreenPos.y/h - 0.5f)/r) * Mathf.Rad2Deg;
+                if (mouseWorldPos.z > 0)
+                {
+                    theta = 180 - theta;
+                }
+                Debug.Log("theta : " + theta);
+                float gamma = - Mathf.Asin(2*s*(mouseScreenPos.x/w - 0.5f)/(alpha*r)) * Mathf.Rad2Deg;
+                if (mouseWorldPos.z > 0)
+                {
+                    gamma = 180 - gamma;
+                }
+                Debug.Log("gamma : " + gamma);
+
+                // Apply the adjustment
+                gameObject.transform.rotation = Quaternion.Euler(theta+phi, -gamma-psi, 0);             
+            }
+        }
+    }
+
+
+    // Get world space position of point under mouse cursor
     public Vector3 GetPerspectivePos()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
         Plane plane = new Plane(transform.forward, 0.0f);
         float distance;
         plane.Raycast(ray, out distance);
         return ray.GetPoint(distance);
+    }
+
+    private void DiffSpherical(Vector3 cartCoordsA, Vector3 cartCoordsB, float r, out float diffX, out float diffY){
+        diffX = (Mathf.Asin(cartCoordsA.y/r) - Mathf.Asin(cartCoordsB.y/r)) * Mathf.Rad2Deg;
+        diffY = (Mathf.Asin(cartCoordsA.x/r) - Mathf.Asin(cartCoordsB.x/r)) * Mathf.Rad2Deg;
     }
 
 }
--- julia_template.jl
+++ julia_template.jl
@@ -10,14 +10,23 @@ plot(x, y, 0, 2*pi)
 =#
 
 #sinus curve
-x(t) = sin(t)
-plot(x, 0, 2*pi, linecolor=:red)
+x(t) = 0
+plot(x, -pi, pi, linecolor=:red)
+x(t) = pi/4
+plot!(x, -pi, pi, linecolor=:red)
+x(t) = -pi/4
+plot!(x, -pi, pi, linecolor=:red)
+vline!([-pi/4, 0, pi/4], linecolor=:red)
+
+
 
 #use these attributes
 plot!(dpi = 1000, framestyle=:none, fmt=:png, background=false, legend=false, grid=false, axis=false, border=false, size=(1000,500), margin=0mm)
+scatter!((-pi, -pi/2), color = "white", label = "", markersize = 0.1)
+scatter!((pi, pi/2), color = "white", label = "", markersize = 0.1)
 
 #save the plot in desired path
-img_path = "doc/img/output.png"
+img_path = "doc/img/help.png"
 savefig(img_path)
 
 #these lines crop the image's margins
